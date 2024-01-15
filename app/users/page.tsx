857d03e8b5831112bdbf90453b7b3f23e07a30bd
Caitlin Swickard <caitlin6029@gmail.com>
2024-01-15 16:46:49 -0700

    ran build to dynamic render timestamp

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 
 // defining the shape of our user object
 interface User {
-  id:number;
-  name:string;
+  id: number;
+  name: string;
 }
 
 // server component
 const UsersPage = async () => {
   // fetch for placeholder users
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users",
+    { cache: 'no-store'}
+  );
   const users: User[] = await res.json();
   return (
     <>
-    <h1>Users</h1>
-    <ul>
-      {users.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
